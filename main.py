86a4709fe403d9d6baf395ef51c38ef29b9bc880
Vladimir <vladimir.volobuev2k02@mail.ru>
2025-04-01 11:17:53 +0100

    Добавлена модель для распознавания машин

--- main.py
+++ main.py
@@ -22,53 +22,6 @@ def list_tflite_devices():
 list_tflite_devices()
 print(tf.config.list_physical_devices('GPU'))
 
-
-def load_pb_graph(modelpath):
-    tf.config.optimizer.set_jit(True)
-    with tf.io.gfile.GFile(modelpath, "rb") as f:
-        graph_def = tf.compat.v1.GraphDef()
-        graph_def.ParseFromString(f.read())
-
-    with tf.Graph().as_default() as graph:
-        tf.import_graph_def(graph_def, name="")
-    return graph
-
-
-def detect_pb_model(image, sess):
-    need_classes = [1]
-    img_h, img_w, _ = image.shape
-    start_time = time.time()
-    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (224, 224))
-
-    inputs = np.reshape(img, (1, 224, 224, 3))
-
-    out = sess.run(
-        [
-            sess.graph.get_tensor_by_name('num_detections:0'),
-            sess.graph.get_tensor_by_name('detection_scores:0'),
-            sess.graph.get_tensor_by_name('detection_boxes:0'),
-            sess.graph.get_tensor_by_name('detection_classes:0')
-        ],
-        feed_dict={'image_tensor:0': inputs}
-    )
-
-    num_detections = int(out[0][0])
-    boxes = out[2][0][:num_detections]
-    scores = out[1][0][:num_detections]
-    class_ids = out[3][0][:num_detections]
-
-    valid_detections = scores > 0.8
-
-    end_time = time.time()
-
-    valid_class_ids = np.array([cid in need_classes for cid in class_ids], dtype=bool)
-    valid_combined = valid_detections & valid_class_ids
-    filtered_class_ids = class_ids[valid_combined]
-
-    return len(filtered_class_ids), end_time - start_time
-
-
 def detect_image_with_model(modelpath, image, height, width):
     interpreter = tf.lite.Interpreter(model_path=modelpath)
     interpreter.allocate_tensors()
@@ -99,28 +52,26 @@ def tflite_detect_images(models, imgpath='TestImages'):
     object_counts = {name: [] for name in models.keys()}
 
     table = PrettyTable(["Файл", *models.keys()])
-    pb_graph = load_pb_graph('frozen_inference_graph_old.pb')
-    with tf.compat.v1.Session(graph=pb_graph) as sess:
-        for image_path in images:
-            image = cv2.imread(image_path)
-            row = [os.path.basename(image_path)]
+    for image_path in images:
+        image = cv2.imread(image_path)
+        row = [os.path.basename(image_path)]
 
-            for name, model in models.items():
-                if model.endswith('.tflite'):
-                    interpreter = tf.lite.Interpreter(model_path=model)
-                    interpreter.allocate_tensors()
-                    input_details = interpreter.get_input_details()
-                    height, width = input_details[0]['shape'][1:3]
-                    objects, time_spent = detect_image_with_model(model, image, height, width)
-                else:
-                    objects, time_spent = detect_pb_model(image, sess)
+        for name, model in models.items():
+            if model.endswith('.tflite'):
+                interpreter = tf.lite.Interpreter(model_path=model)
+                interpreter.allocate_tensors()
+                input_details = interpreter.get_input_details()
+                height, width = input_details[0]['shape'][1:3]
+                objects, time_spent = detect_image_with_model(model, image, height, width)
+            else:
+                continue
 
-                fps = 1 / time_spent if time_spent > 0 else 0
-                row.append(f"{objects} ob, {fps:.2f} FPS")
-                results[name].append([time_spent, fps])
-                object_counts[name].append(objects)
+            fps = 1 / time_spent if time_spent > 0 else 0
+            row.append(f"{objects} ob, {fps:.2f} FPS")
+            results[name].append([time_spent, fps])
+            object_counts[name].append(objects)
 
-            table.add_row(row)
+        table.add_row(row)
     print(table)
 
     avg_results = {name: np.mean(vals, axis=0) for name, vals in results.items()}
@@ -142,9 +93,10 @@ def tflite_detect_images(models, imgpath='TestImages'):
 models = {
     "224x224": "detect.tflite",
     "quant 224x224": "detect_quant.tflite",
-    "320x320": "detect320.tflite",
-    "quant 320x320": "detect_quant320.tflite",
-    "old model 224x224": "frozen_inference_graph_old.pb"
+    #"320x320": "detect320.tflite",
+    #"quant 320x320": "detect_quant320.tflite"
+    "mn_car_224": "mn_car_224.tflite",
+    "mn_car_224_quant": "mn_car_224_quant.tflite"
 }
 
 tflite_detect_images(models)
