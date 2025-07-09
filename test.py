31ab23a8f940c8de0e79be3ef6f82e39a62f1245
Vladimir <vladimir.volobuev2k02@mail.ru>
2025-07-10 00:00:40 +0100

    Добавлена YOLO n 320

--- test.py
+++ test.py
@@ -1,19 +1,31 @@
 import os
-from ultralytics import YOLO
+import time
 import logging
+from ultralytics import YOLO  # Замените на правильный импорт
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s - %(filename)s - %(lineno)d'
-)
 logger = logging.getLogger()
+image_folder = "images"
+models_folder = "models"
 
-image_folder = "TestImages"
+total_time = 0
+count = 0
 
-onnx_model = YOLO("best.onnx")
-logger.log(logging.WARNING, 'ONNX')
+for model_file in os.listdir(models_folder):
+    count = 0
+    total_time = 0
+    model_path = os.path.join(models_folder, model_file)
+    onnx_model = YOLO(model_path)
+    logger.log(logging.WARNING, f'Model: {model_file}')
 
-for filename in os.listdir(image_folder):
-    if filename.endswith(('.png', '.jpg', '.jpeg', '.bmp')):
-        image_path = os.path.join(image_folder, filename)
-        results = onnx_model(image_path)
+    for filename in os.listdir(image_folder):
+        if filename.endswith(('.png', '.jpg', '.jpeg', '.bmp')):
+            image_path = os.path.join(image_folder, filename)
+            start_time = time.time()
+            results = onnx_model(image_path, verbose=False)
+            total_time += time.time() - start_time
+            count += 1
+    if count > 0:
+        average_time = total_time / count
+        logger.log(logging.WARNING, f'Average time: {average_time:.4f} seconds')
+        logger.log(logging.WARNING, f'Inferences per second: {1 / average_time:.2f}\n')
+
