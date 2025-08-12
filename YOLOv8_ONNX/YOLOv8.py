18c3261bcaeb0e4d344ffbaec9bb363947ec002e
Vladimir <vladimir.volobuev2k02@mail.ru>
2025-08-12 20:43:15 +0100

    Добавлена новая модель

--- YOLOv8_ONNX/YOLOv8.py
+++ YOLOv8_ONNX/YOLOv8.py
@@ -20,7 +20,7 @@ class YOLOv8:
 
     def initialize_model(self, path):
         self.session = onnxruntime.InferenceSession(path,
-                                                    providers=['AzureExecutionProvider'])
+                                                    providers=['CUDAExecutionProvider'])
         # Get model info
         self.get_input_details()
         self.get_output_details()
